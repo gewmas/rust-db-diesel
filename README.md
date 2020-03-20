377cad3a0fe4601f15293e86142f6537b67717e4
Yuhua Mai <yuhua@urbandoor.com>
2020-03-20 00:01:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Diesel
+
+## Commands
+
+```bash
+diesel migration run
+diesel migration redo
+
+cargo run --bin show_posts
+```
+
+## Reference
+
+- [Diesel Getting Started](http://diesel.rs/guides/getting-started/)
---
+++ src/bin/show_posts.rs
@@ -0,0 +1,23 @@
+extern crate diesel;
+
+use self::diesel::prelude::*;
+use rust_db_diesel::models::*;
+use rust_db_diesel::*;
+
+fn main() {
+    use rust_db_diesel::schema::posts::dsl::*;
+
+    let connection = establish_connection();
+    let results = posts
+        .filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(&connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("----------\n");
+        println!("{}", post.body);
+    }
+}
